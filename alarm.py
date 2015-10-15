4c698ef1758967d2e321cbdaedb5f9640e88f7a4
Alejandro Varas <alej0varas@gmail.com>
2015-10-15 18:18:15 +0200

    Made delta configurable

--- alarm.py
+++ alarm.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 from random import choice, randint
 
 
+DEFAULT_DELTA_MINUTES = 10
 VOLUME_INCREMENT_RATE = 60  # in seconds
 VOLUME_INCREMENT = 5  # as a percentage
 VOLUME_MAX_VALUE = 100  # as a percentage
@@ -17,16 +18,14 @@ VOLUME_INCREMENT_COMMAND = 'pactl set-sink-volume 1 +%s%%' % VOLUME_INCREMENT
 VOLUME_TO_MINIMUM_COMMAND = 'pactl set-sink-volume 1 0%'
 
 REFRESH_RATE = 60  # in seconds
-DELTA_SIZE = 10  # in minutes
-RANDOM_DELTA = randint(0, DELTA_SIZE * 60) * choice((-1, 1))
 
 MACRO_FILE_PATH = 'autoplay-spotify.cnee'
 ALARM_COMMAND = '/usr/bin/cnee --replay -f %s' % MACRO_FILE_PATH
 
 
-def main(sleep_time_hours, sleep_time_minutes):
-
-    sleep_time = sleep_time_hours * 60 * 60 + sleep_time_minutes * 60 + RANDOM_DELTA
+def main(sleep_time_hours, sleep_time_minutes, delta):
+    random_delta = randint(0, delta * 60) * choice((-1, 1))
+    sleep_time = sleep_time_hours * 60 * 60 + sleep_time_minutes * 60 + delta
 
     # It can be done with this two lines only but the following is
     # more fancy
@@ -52,9 +51,11 @@ def main(sleep_time_hours, sleep_time_minutes):
 
 
 if __name__ == "__main__":
-    sleep_time_hours, sleep_time_minutes = 6, 0
+    sleep_time_hours, sleep_time_minutes, delta = 6, 0, DEFAULT_DELTA_MINUTES
     if len(sys.argv) > 1:
         sleep_time_hours = int(sys.argv[1])
     if len(sys.argv) > 2:
         sleep_time_minutes = int(sys.argv[2])
-    main(sleep_time_hours, sleep_time_minutes)
+    if len(sys.argv) > 3:
+        delta = int(sys.argv[3])
+    main(sleep_time_hours, sleep_time_minutes, delta)
